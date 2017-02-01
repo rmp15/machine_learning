ebb2e6519680b112033727c688b7a5d18b3521a4
rmp15 <r.parks15@imperial.ac.uk>
2017-02-01 10:51:10 +0000

    Updates file paths

--- multi_var_regression/scripts/csv_test.py
+++ multi_var_regression/scripts/csv_test.py
@@ -1,8 +1,13 @@
 import pandas as pd
-
+import seaborn as sns
 from data.file_paths import *
 
+# load data
 dat = pd.read_csv(TEMP_TEST_1, index_col=0)
 
+# print data
 print(dat)
+
+# plot data using seaborn?
+sinplot()
 
