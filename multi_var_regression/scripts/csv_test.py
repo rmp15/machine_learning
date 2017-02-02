26e7b43094de837706f2696ed06f4d13ccd10aec
rmp15 <r.parks15@imperial.ac.uk>
2017-02-02 19:50:37 +0000

    Updates to file organisation

--- multi_var_regression/scripts/csv_test.py
+++ multi_var_regression/scripts/csv_test.py
@@ -1,8 +1,9 @@
 import pandas as pd
-import seaborn as sns
 from data.file_paths import *
 
 # load data
+from multi_var_regression.data.file_paths import TEMP_TEST_1
+
 dat = pd.read_csv(TEMP_TEST_1, index_col=0)
 
 # print data
