b8959507273a4ad0687afff3cf00aed1859d7f13
rmp15 <r.parks15@imperial.ac.uk>
2017-02-02 16:51:06 +0000

    Updates file paths

--- multi_var_regression/functions/plot/scatplot_facet.py
+++ multi_var_regression/functions/plot/scatplot_facet.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def scatplot_facet(x, y, facet_row, facet_col, hue, data):
+def scatplot_facet_1(x, y, facet_row, facet_col, hue, data):
 
     temp = sns.FacetGrid(data=data, row=facet_row, col=facet_col, hue=hue, col_wrap=10)
     temp.map(plt.scatter, x, y, edgecolor="w")
--- multi_var_regression/scripts/scatplot_test_2.py
+++ multi_var_regression/scripts/scatplot_test_2.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
 from data.file_paths import *
-from functions.plot import scatplot_facet
+from functions.plot import scatplot_facet_1
 
 dat = pd.read_csv(TEMP_TEST_1, index_col=0, nrows=1000)
 
 # create unique id based on year and month
-dat['year_month'] = dat.year.astype(str) + '_' + dat.month.astype(str)
+#dat['year_month'] = dat.year.astype(str) + '_' + dat.month.astype(str)
 
 # plot data factored
-scatplot_facet('year', 'rate.adj', facet_row='age', facet_col='state.name', hue='month', data=dat)
+scatplot_facet_1('year', 'rate.adj', facet_row='age', facet_col='state.name', hue='month', data=dat)
+#scatplot_facet()
