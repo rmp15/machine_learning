09f470adbaa7a204fd0e50a9f22f3802495905f3
rmp15 <r.parks15@imperial.ac.uk>
2017-02-02 18:59:16 +0000

    Updates file paths

--- multi_var_regression/functions/plot/scatplot_facet.py
+++ multi_var_regression/functions/plot/scatplot_facet.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+from matplotlib.pyplot import savefig
 
 
 def scatplot_facet(x, y, facet_col, data, hue, title_main, title_sub, x_label, y_label, col_wrap=5):
@@ -11,12 +12,13 @@ def scatplot_facet(x, y, facet_col, data, hue, title_main, title_sub, x_label, y
     temp.map(plt.scatter, x, y, edgecolor="w")
 
     # adjust main title position to not overlap
-    plt.subplots_adjust(top=0.2)
+    #plt.subplots_adjust(top=0.2)
 
     # add plot details
     temp.fig.suptitle(title_main)
     temp.set_axis_labels(x_label, y_label)
-    temp.set_titles(str(title_sub))
-    temp.add_legend()
+    temp.set_titles(title_sub)
+    # temp.add_legend()
 
-    plt.show()
+    #plt.show()
+    savefig('../output/test.pdf', bbox_inches='tight')
--- multi_var_regression/scripts/scatplot_test_2.py
+++ multi_var_regression/scripts/scatplot_test_2.py
@@ -1,25 +1,30 @@
 import pandas as pd
+import numpy as np
 
 from data.file_paths import *
 from functions.plot.scatplot_facet import scatplot_facet
 
-dat = pd.read_csv(TEMP_TEST_1, index_col=0)
+# load data
+dat = pd.read_csv(TEMP_TEST_1, index_col=0,nrows=1000)
 
-# create unique id based on year and month
+# create unique id based on year and month MAKE FUNCTION
 dat['year_month'] = dat.year.astype(str) + '_' + dat.month.astype(str)
 
-# create death rate per 100,000
+# create death rate per 100,000 MAKE FUNCTION
 dat['rate_100000'] = dat['rate.adj'] * 100000
 
-# rename column names
+# rename column names MAKE FUNCTION
 dat.rename(columns={'state.name': 'state_name'}, inplace=True)
 
-# unique names of states
+# unique names of states MAKE FUNCTION
 state_names = dat.state_name.unique()
+state_names = state_names.tolist()
 
 print(state_names)
 
+print(type(state_names))
+
 # plot data factored
 scatplot_facet(x='year', y='rate_100000', facet_col='state_name',
-                 hue='month', data=dat, title_main='Death rates by state', title_sub='',
+                 hue='month', data=dat, title_main='Death rates by state',title_sub=dat.state_name.unique(),
                  x_label='Year', y_label='Death rate (per 100,000)')
