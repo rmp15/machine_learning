a0a0881542a10058f33cc166087889353b10f03a
rmp15 <r.parks15@imperial.ac.uk>
2017-02-02 20:33:54 +0000

    Updates to file organisation

--- multi_var_regression/scripts/scatplot_test_2.py
+++ multi_var_regression/scripts/scatplot_test_2.py
@@ -8,9 +8,13 @@ dat = pd.read_csv(TEMP_TEST_1, index_col=0)
 
 # create unique id based on year and month MAKE FUNCTION
 dat['year_month'] = dat.year.astype(str) + dat.month.astype(str)
+dat['year_month'] = pd.factorize(dat.year_month)[0] + 1
 
-# create death rate per 100,000
-rate_multiply(dat, 'rate.adj', 1000000)
+print(dat)
+
+# create death rate per x
+per_num = 100000
+rate_multiply(dat, 'rate.adj', per_num)
 
 # rename column names
 dat.rename(columns={'state.name': 'state_name'}, inplace=True)
@@ -24,6 +28,6 @@ print(state_names)
 print(type(state_names))
 
 # plot data factored
-scatplot_facet(x='year', y='rate_1000000', facet_col='state_name',
+scatplot_facet(x='year_month', y=('rate_'+str(per_num)), facet_col='state_name',
                  hue='month', data=dat, title_main='Death rates by state',
-                 x_label='Year', y_label='Death rate (per 1,000,000)')
+                 x_label='Year', y_label='Death rate (per '+str(per_num)+')')
