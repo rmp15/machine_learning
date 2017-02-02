4650e460c21318e10f99cd7873e58008cb747c83
rmp15 <r.parks15@imperial.ac.uk>
2017-02-02 16:39:20 +0000

    Updates file paths

--- multi_var_regression/functions/plot/scatplot_facet.py
+++ multi_var_regression/functions/plot/scatplot_facet.py
@@ -4,7 +4,7 @@ import seaborn as sns
 
 def scatplot_facet(x, y, facet_row, facet_col, hue, data):
 
-    temp = sns.FacetGrid(data=data, row=facet_row, col=facet_col, hue=hue)
+    temp = sns.FacetGrid(data=data, row=facet_row, col=facet_col, hue=hue, col_wrap=10)
     temp.map(plt.scatter, x, y, edgecolor="w")
 
     plt.show()
