cff46c8b233d1fd9da8a2697bcd879b968ca44a4
rmp15 <r.parks15@imperial.ac.uk>
2017-02-02 17:47:30 +0000

    Updates file paths

--- multi_var_regression/functions/plot/scatplot_facet.py
+++ multi_var_regression/functions/plot/scatplot_facet.py
@@ -2,9 +2,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def scatplot_facet_1(x, y, facet_row, facet_col, hue, data):
+def scatplot_facet_1(x, y, facet_col, hue, data, title_main, title_sub, x_label, y_label):
 
-    temp = sns.FacetGrid(data=data, row=facet_row, col=facet_col, hue=hue, col_wrap=10)
+    # create facet grid
+    temp = sns.FacetGrid(data=data, col=facet_col, hue=hue, col_wrap=5)
+
+    # create scatter plot
     temp.map(plt.scatter, x, y, edgecolor="w")
+
+    # adjust main title position to not overlap
+    plt.subplots_adjust(top=0.9)
+
+    # add plot details
+    temp.fig.suptitle(title_main)
+    temp.set_axis_labels(x_label, y_label)
+    temp.set_titles(title_sub)
+    temp.add_legend()
 
     plt.show()
--- multi_var_regression/scripts/scatplot_test_2.py
+++ multi_var_regression/scripts/scatplot_test_2.py
@@ -1,13 +1,25 @@
 import pandas as pd
 
 from data.file_paths import *
-from functions.plot import scatplot_facet_1
+from functions.plot.scatplot_facet import scatplot_facet_1
 
-dat = pd.read_csv(TEMP_TEST_1, index_col=0, nrows=1000)
+dat = pd.read_csv(TEMP_TEST_1, index_col=0, nrows=2000)
 
 # create unique id based on year and month
-#dat['year_month'] = dat.year.astype(str) + '_' + dat.month.astype(str)
+dat['year_month'] = dat.year.astype(str) + '_' + dat.month.astype(str)
+
+# create death rate per 100,000
+dat['rate_100000'] = dat['rate.adj'] * 100000
+
+# rename column names
+dat.rename(columns={'state.name': 'state_name'}, inplace=True)
+
+# unique names of states
+state_names = dat.state_name.unique()
+
+print(state_names)
 
 # plot data factored
-scatplot_facet_1('year', 'rate.adj', facet_row='age', facet_col='state.name', hue='month', data=dat)
-#scatplot_facet()
+scatplot_facet_1(x='year', y='rate_100000', facet_col='state_name',
+                 hue='month', data=dat, title_main='Death rates by state', title_sub='',
+                 x_label='Year', y_label='Death rate (per 100,000)')
