5cd6826f8fb2cd3956d6c4f7c212b5bd8ed18e16
rmp15 <r.parks15@imperial.ac.uk>
2017-02-03 11:47:38 +0000

    Tidied up

--- multi_var_regression/functions/plot/plot_tools.py
+++ multi_var_regression/functions/plot/plot_tools.py
@@ -52,40 +52,21 @@ def scatplot_facet(x, y, facet_col, data, hue, title_main, x_label, y_label, out
 
 
 # plots line plot coloured and faceted
-#def linplot_facet(x, y, facet_col, data, hue, title_main, x_label, y_label, output, col_wrap=5, ):
-def linplot_facet(x, y, data, **kwargs):
+def plot_facet(plot, x, y, facet_col, data, title_main, x_label, y_label, output, col_wrap=5, hue=None):
 
     # create facet grid
-    if 'hue' in kwargs:
-        temp = sns.FacetGrid(data=data, col=facet_col, hue=hue, col_wrap=col_wrap)
-    else:
+    if 'hue' is None:
         temp = sns.FacetGrid(data=data, col=facet_col, col_wrap=col_wrap)
+    else:
+        temp = sns.FacetGrid(data=data, col=facet_col, hue=hue, col_wrap=col_wrap)
 
-    # create scatter plot
-    temp.map(plt.plot, x, y,)
-
-    # adjust main title position to not overlap
-    # plt.subplots_adjust(top=0.01)
-
-    # add plot details
-    temp.fig.suptitle(title_main)
-    temp.set_axis_labels(x_label, y_label)
-    temp.set_titles('{col_name}')
-    temp.add_legend()
-
-    # plt.show()
-    rc('figure', figsize=(11.69, 8.27))
-    savefig('../output/' + output + '.pdf', bbox_inches='tight')
-
-
-# plots line plot coloured and faceted DELETE WHEN FIXED ABOVE
-def linplot_facet_2(x, y, facet_col, data, title_main, x_label, y_label, output, col_wrap=5, ):
-
-    # create facet grid
-    temp = sns.FacetGrid(data=data, col=facet_col, col_wrap=col_wrap)
-
-    # create scatter plot
-    temp.map(plt.plot, x, y,)
+    # create plot
+    if plot is 'scatter':
+        temp.map(plt.scatter, x, y)
+    elif plot is 'line':
+        temp.map(plt.plot, x, y)
+    else:
+        print('Please specify plot type with plot argument')
 
     # adjust main title position to not overlap
     # plt.subplots_adjust(top=0.01)
--- multi_var_regression/scripts/plot_test.py
+++ multi_var_regression/scripts/plot_test.py
@@ -1,10 +1,8 @@
-import pandas as pd
-
 from multi_var_regression.data.file_paths import TEMP_TEST_2
 from multi_var_regression.functions.data_manip.data_tools import *
-from multi_var_regression.functions.plot.plot_tools import scatplot_facet, linplot_facet, linplot_facet_2
+from multi_var_regression.functions.plot.plot_tools import plot_facet
 
-dat = pd.read_csv(TEMP_TEST_2, index_col=0, nrows=1000)
+dat = pd.read_csv(TEMP_TEST_2, index_col=0)
 
 # create unique id based on year and month FIX FUNCTION
 #compound_key(dat, 'year', 'month')
@@ -19,17 +17,17 @@ rate_multiply(dat, 'rate.adj', per_num)
 dat.rename(columns={'state.name': 'state_name'}, inplace=True)
 
 # scatter plot of data factored by month and faceted by state
-scatplot_facet(x='year_month', y=('rate_'+str(per_num)), data=dat, facet_col='state_name',
-                 hue='month', title_main='Death rates by state',
-                 x_label='Time', y_label='Death rate (per '+str(per_num)+')', output='scatplot_facet_1')
+plot_facet(plot='scatter', x='year_month', y=('rate_'+str(per_num)), data=dat, facet_col='state_name',
+           hue='month', title_main='Death rates by state coloured by month',
+           x_label='Time', y_label='Death rate (per '+str(per_num)+')', output='scatplot_facet_1')
 
 # line plot of data factored by month and faceted by state
-linplot_facet(x='year_month', y=('rate_'+str(per_num)), data=dat,
-                 facet_col='state_name', hue='month', title_main='Death rates by state',
-                 x_label='Time', y_label='Death rate (per '+str(per_num)+')', output='linplot_facet_1')
+plot_facet(plot='line', x='year_month', y=('rate_'+str(per_num)), data=dat,
+           facet_col='state_name', hue='month', title_main='Death rates by state coloured by month',
+           x_label='Time', y_label='Death rate (per '+str(per_num)+')', output='linplot_facet_1')
 
 # line plot of data faceted by state only
-linplot_facet(x='year_month', y=('rate_'+str(per_num)), data=dat,
-                facet_col='state_name', title_main='Death rates by state',
-                x_label='Time', y_label='Death rate (per '+str(per_num)+')', output='linplot_facet_2')
+plot_facet(plot='line', x='year_month', y=('rate_'+str(per_num)), data=dat,
+           facet_col='state_name', title_main='Death rates by state',
+           x_label='Time', y_label='Death rate (per '+str(per_num)+')', output='linplot_facet_2')
 
