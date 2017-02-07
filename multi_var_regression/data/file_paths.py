9c8bdbe6ee52d34f647564f26eca852a98f8e47c
rmp15 <r.parks15@imperial.ac.uk>
2017-02-07 16:24:58 +0000

    Tidied up

--- multi_var_regression/data/file_paths.py
+++ multi_var_regression/data/file_paths.py
@@ -1,5 +1,6 @@
-# Data sets
 import os
+
+# Data sets
 
 IRIS_TRAINING = "/Users/rmiparks/git/machine_learning/multi_var_regression/data/iris_training.csv"
 IRIS_TEST = "/Users/rmiparks/git/machine_learning/multi_var_regression/data/iris_test.csv"
--- multi_var_regression/scripts/plot_test.py
+++ multi_var_regression/scripts/plot_test.py
@@ -1,11 +1,13 @@
+import os
+
 from multi_var_regression.data.file_paths import TEMP_TEST_2
 from multi_var_regression.functions.data_manip.data_tools import *
 from multi_var_regression.functions.plot.plot_tools import plot_facet
 
-dat = pd.read_csv(TEMP_TEST_2, index_col=0)
+dat = pd.read_csv(TEMP_TEST_2, index_col=0, nrows=1000)
 
 # create unique id based on year and month FIX FUNCTION
-#compound_key(dat, 'year', 'month')
+# compound_key(dat, 'year', 'month')
 dat['year_month'] = dat.year.astype(str) + dat.month.astype(str)
 dat['year_month'] = pd.factorize(dat.year_month)[0] + 1
 
@@ -16,18 +18,24 @@ rate_multiply(dat, 'rate.adj', per_num)
 # rename column names
 dat.rename(columns={'state.name': 'state_name'}, inplace=True)
 
+# create file string for output
+output_loc = os.path.join('../output/')
+
+# create directory if it doesn't exist
+if not os.path.exists(output_loc):
+    os.makedirs(output_loc)
+
 # scatter plot of data factored by month and faceted by state
-plot_facet(plot='scatter', x='year_month', y=('rate_'+str(per_num)), data=dat, facet_col='state_name',
+plot_facet(plot='scatter', x='year_month', y=('rate_' + str(per_num)), data=dat, facet_col='state_name',
            hue='month', title_main='Death rates by state coloured by month',
-           x_label='Time', y_label='Death rate (per '+str(per_num)+')', output='scatplot_facet_1')
+           x_label='Time', y_label='Death rate (per ' + str(per_num) + ')', output=output_loc + 'scatplot_facet_1')
 
 # line plot of data factored by month and faceted by state
-plot_facet(plot='line', x='year_month', y=('rate_'+str(per_num)), data=dat,
+plot_facet(plot='line', x='year_month', y=('rate_' + str(per_num)), data=dat,
            facet_col='state_name', hue='month', title_main='Death rates by state coloured by month',
-           x_label='Time', y_label='Death rate (per '+str(per_num)+')', output='linplot_facet_1')
+           x_label='Time', y_label='Death rate (per ' + str(per_num) + ')', output=output_loc + 'linplot_facet_1')
 
 # line plot of data faceted by state only
-plot_facet(plot='line', x='year_month', y=('rate_'+str(per_num)), data=dat,
+plot_facet(plot='line', x='year_month', y=('rate_' + str(per_num)), data=dat,
            facet_col='state_name', title_main='Death rates by state',
-           x_label='Time', y_label='Death rate (per '+str(per_num)+')', output='linplot_facet_2')
-
+           x_label='Time', y_label='Death rate (per ' + str(per_num) + ')', output=output_loc + 'linplot_facet_2')
