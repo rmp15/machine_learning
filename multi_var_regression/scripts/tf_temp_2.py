31a57379826a2f0f78c97500ff1fff9036cc62ce
rmp15 <r.parks15@imperial.ac.uk>
2017-02-08 14:46:45 +0000

    updated machine learning

--- multi_var_regression/scripts/tf_temp_2.py
+++ multi_var_regression/scripts/tf_temp_2.py
@@ -16,9 +16,9 @@ from multi_var_regression.data.file_paths import *
 
 # details of data set
 COLUMNS = ['sex', 'age', 'year', 'month', 'fips', 'deaths', 'iso3', 'pop', 'pop.adj', 'rate',
-           'rate.adj', 'rate.adj.old', 'leap', 'deaths.adj', 'variable', 'month.short', 'state.name']
+           'rate.adj', 'rate.adj.old', 'leap', 'deaths.adj', 'variable', 'month.short', 'state.name', 'season']
 FEATURES = ['year', 'month', 'variable']
-LABEL = 'label'
+LABEL = 'season'
 
 CONTINUOUS_COLUMNS = ['deaths', 'pop', 'year', 'pop.adj', 'rate', 'rate.adj', 'rate.adj.old', 'deaths.adj', 'variable']
 CATEGORICAL_COLUMNS = ['sex', 'age', 'month', 'fips', 'iso3', 'leap', 'month.short', 'state.name']
@@ -44,34 +44,34 @@ pop_adj = tf.contrib.layers.real_valued_column("pop.adj")
 rate_adj = tf.contrib.layers.real_valued_column("rate.adj")
 temperature = tf.contrib.layers.real_valued_column("variable")
 
-print(deaths_adj)
-
 # create function to process data set
 def input_fn(df):
 
     # Creates a dictionary mapping from each continuous feature column name (k) to
     # the values of that column stored in a constant Tensor.
     continuous_cols = {k: tf.constant(df[k].values)
-                         for k in CONTINUOUS_COLUMNS}
+                       for k in CONTINUOUS_COLUMNS}
     # Creates a dictionary mapping from each categorical feature column name (k)
     # to the values of that column stored in a tf.SparseTensor.
     categorical_cols = {k: tf.SparseTensor(
         indices=[[i, 0] for i in range(df[k].size)],
         values=df[k].values,
         shape=[df[k].size, 1])
-                          for k in CATEGORICAL_COLUMNS}
+                        for k in CATEGORICAL_COLUMNS}
     # Merges the two dictionaries into one.
     feature_cols = dict(continuous_cols.items() + categorical_cols.items())
     # Converts the label column into a constant Tensor.
-    label = tf.constant(df[LABEL_COLUMN].values)
+    label = tf.constant(df[LABEL].values)
     # Returns the feature columns and the label.
     return feature_cols, label
 
+
 def train_input_fn():
-  return input_fn(df_train)
+    return input_fn(training_set)
+
 
 def eval_input_fn():
-  return input_fn(df_test)
+    return input_fn(test_set)
 
 # defining the logistic regression model
 model_dir = tempfile.mkdtemp()
@@ -80,6 +80,4 @@ m = tf.contrib.learn.LinearClassifier(feature_columns=[sex, age, month, fips, ra
                                       model_dir=model_dir)
 
 # train and evaluate model
-m.fit(input_fn=train_input_fn, steps=200)
-
-
+m.fit(input_fn=train_input_fn(), steps=200)
