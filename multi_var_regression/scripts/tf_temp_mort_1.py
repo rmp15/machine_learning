dbee48f05c05744e47a723732a01d07789a68919
rmp15 <r.parks15@imperial.ac.uk>
2017-02-08 12:03:34 +0000

    Fixed function

--- multi_var_regression/scripts/tf_temp_mort_1.py
+++ multi_var_regression/scripts/tf_temp_mort_1.py
@@ -5,20 +5,23 @@ from __future__ import print_function
 import numpy as np
 import tensorflow as tf
 import pandas as pd
+import itertools
 
 from multi_var_regression.data.file_paths import TEMP_MORT
 
 # details of data set
 COLUMNS = ['sex', 'age', 'year', 'month', 'fips', 'deaths', 'iso3', 'pop', 'pop.adj', 'rate',
            'rate.adj', 'rate.adj.old', 'leap', 'deaths.adj', 'variable', 'month.short', 'state.name']
-FEATURES = ['year', 'month', 'fips']
-LABEL = 'variable'
+FEATURES = ['year', 'month', 'variable']
+LABEL = 'rate.adj'
 
 # Load data sets
 training_set = pd.read_csv(TEMP_MORT, skipinitialspace=True,
                            skiprows=1, names=COLUMNS)
 test_set = pd.read_csv(TEMP_MORT, skipinitialspace=True,
                        names=COLUMNS, skiprows=(training_set.shape[0] - 1000 + 1))
+prediction_set = pd.read_csv(TEMP_MORT, skipinitialspace=True,
+                       names=COLUMNS, skiprows=(training_set.shape[0] - 10 + 1))
 
 # create feature columns formally, confirming they are all real-valued
 feature_cols = [tf.contrib.layers.real_valued_column(k)
@@ -38,3 +41,16 @@ def input_fn(data_set):
 
 # train the neural network regressor
 regressor.fit(input_fn=lambda: input_fn(training_set), steps=5000)
+
+# evaluate the model fit
+ev = regressor.evaluate(input_fn=lambda: input_fn(test_set), steps=1)
+loss_score = ev["loss"]
+print("Loss: {0:f}".format(loss_score))
+
+# make predictions using the model
+y = regressor.predict(input_fn=lambda: input_fn(prediction_set))
+# .predict() returns an iterator; convert to a list and print predictions
+predictions = list(itertools.islice(y, 10))
+print("Predictions: {}".format(str(predictions)))
+
+print(prediction_set.head())
