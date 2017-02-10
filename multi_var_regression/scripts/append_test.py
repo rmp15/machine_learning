7069862d64f9cf6fc5a497ace61ccbf0c19caa9f
rmp15 <r.parks15@imperial.ac.uk>
2017-02-10 14:33:17 +0000

    created data frame appender

--- multi_var_regression/scripts/append_test.py
+++ multi_var_regression/scripts/append_test.py
@@ -2,4 +2,4 @@ from multi_var_regression.data.categorical import ages, sexes_long_2
 from multi_var_regression.data.file_paths import TEMP_TEST, TEMP_TEST_B
 from multi_var_regression.functions.data_manip.data_tools import data_append
 
-test = data_append(TEMP_TEST_B, '1982_2013', ages, sexes_long_2, ages, )
+test = data_append(TEMP_TEST_B, '1982_2013', ages, sexes_long_2, ages, 'mort_against_climate_85_1_1982_2013_t2m_mean.csv')
